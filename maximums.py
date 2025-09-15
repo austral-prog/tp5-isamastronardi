86fdeacf8f4cc4ba31db39fdf07072c70f0f4446
isamastronardi <imastronardibelpoli@mail.austral.edu.ar>
2025-09-15 12:38:47 -0300

    Add files via upload

--- maximums.py
+++ maximums.py
@@ -2,9 +2,25 @@
 
 def max_of_two(x, y):
     """Given x and y, that are 2 numbers, return the biggest number."""
-    return "ANSWER HERE" # Remove this line and implement
-
+    if x > y:
+        return x
+    elif x < y:
+        return y
+    else:
+        return 0
+maximo_total = max_of_two(9,5)
+#print(f"Retorna: {maximo_total}")
 
 def max_of_three(x, y, z):
     """Given x, y and z, that are 3 numbers, return the biggest number of the three."""
-    return "ANSWER HERE" # Remove this line and implement
+    if (x>y) and (x>z):
+        return x
+    elif (y>z) and (y>x):
+        return y
+    elif (z>x) and (z>y):
+        return z
+    else:
+        return 0
+maximo_total_2 = max_of_three(8, 6, 4)
+#print(f"Retorna: {maximo_total_2}")
+   
--- quadratic.py
+++ quadratic.py
@@ -1,16 +1,66 @@
 # Replace the "ANSWER HERE" for your answer
 
-def roots(a, b, c):
-    return "ANSWER HERE"
+def roots(a, b, c): #las roots las guardas primero en una variable de resuktado y dsp que te de el resultado
+    discriminante = (b**2-4*a*c)
+    if discriminante>=0:
+        r1 = (-b+(discriminante**(1/2)))/2*a
+        r2 = (-b-(discriminante**(1/2)))/2*a
+        if r1!=r2:
+            return f"({r1}, {r2})"
+        else:
+            return f"({r1})"
+    else:
+        return f"( )"
+resultado = roots(2, 4, 2)
+#print(f"Retorna: {resultado}")
+
+def value_y(a, b, c, x): 
+    y = a*(x**2)+b*x+c
+    return y
+resultado_2 = value_y(1, 2, 3, 1)
+#print(f"Retorna:{resultado_2}")
 
 
-def value_y(a, b, c, x):
-    return "ANSWER HERE"
-
-
-def to_string(a, b, c):
-    return "ANSWER HERE"
-
+def to_string(a, b, c): 
+    ecuacion = ""
+    if a!=0:
+        a_1=f"{a} * X^2 "
+        ecuacion = a_1
+    if b!=0:
+        if ecuacion=="":
+            b_1=f"{b} * X "
+            ecuacion = b_1
+            #ecuacion din msd
+        else:
+            b_1=f"+ {b} * X "
+            ecuacion = ecuacion + b_1
+            #con mas
+    if c!=0:
+        if ecuacion =="":
+            ecuacion =c
+        else:
+            c_1=f"+ {c}"
+            ecuacion = ecuacion + c_1
+    return f"f(x) = {ecuacion}"
+ecuacion_final = to_string(2, 4, 6)
+print(f"Retorna: {ecuacion_final}")
+    
 
 def derivation(a, b, c):
-    return "ANSWER HERE"
+    resultado_3 = ""
+    if a != 0:
+        a1 = f"{2 * a} * X"
+        resultado_3 = a1
+        if b != 0:
+            b1=f" + {1*b}"
+            resultado_3 = resultado_3 +b1
+        else:
+            resultado_3 = resultado_3
+    else:
+        b1=f"{1*b}"
+        resultado_3 = b1
+    return f"f'(x) = {resultado_3}"
+ecuacion_ultima = derivation(4, 3, 2)
+print(f"Retorna: {ecuacion_ultima}")
+
+
